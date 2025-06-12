e4cbe64e54bb76fa1c54c1d1813adfbd35e10553
cochearno <cochearno17@gmail.com>
2025-06-12 15:35:09 +0200

    seconde partie contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Placeholder } from "react-bootstrap";
 
-const Contact = () => {
+function Contact() {
   return (
     <>
       <div className="titrecont">
@@ -51,10 +50,42 @@ const Contact = () => {
           </article>
           <article className="colonne2 droite2">
             <h3 className="coord">Mes coordonnées</h3>
+            <h5 className="coordjd">John Doe</h5>
+            <div className="mapicon">
+              <i className="bi bi-map" style={{ fontStyle: "normal" }}></i> 40
+              rue Laure Diebold
+            </div>
+            <div className="geoicon">
+              <i className="bi bi-geo-alt" style={{ fontStyle: "normal" }}></i>{" "}
+              69009 Lyon, France
+            </div>
+            <div className="phoneicon">
+              <i className="bi bi-phone" style={{ fontStyle: "normal" }}></i> 10
+              20 30 40 50
+            </div>
+            <div className="mailicon">
+              <i
+                className="bi bi-envelope-at"
+                style={{ fontStyle: "normal" }}
+              ></i>{" "}
+              john.doe@gmail.com
+            </div>
+            <div className="map">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.6271524621793!2d4.796403976516667!3d45.7786619710808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1749734358952!5m2!1sfr!2sfr"
+                width="350"
+                height="300"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="carte rue diebold"
+              />
+            </div>
           </article>
         </section>
       </div>
     </>
   );
-};
+}
 export default Contact;
